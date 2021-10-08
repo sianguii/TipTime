d699dcfa0a7c9c8b5a5c032f5e205a9091e6a1a5
siangui <sisiestudos@gmail.com>
2021-10-07 23:53:23 -0300

    TipTime

---
+++ TipTimee/app/src/main/java/com/example/tiptimee/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tiptimee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiptimee.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = formattedTip
+
+        /*getString(R.string.tip_amount, formattedTip)*/
+    }
+
+}
